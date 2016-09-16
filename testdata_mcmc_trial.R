d83d96e57198f32c266b95cb834badeda9a0c91d
russellturk <russell.turk@live.co.uk>
2016-09-16 09:23:18 +0100

    added lmer model for comparisson purposes

--- testdata_mcmc_trial.R
+++ testdata_mcmc_trial.R
@@ -13,3 +13,11 @@ mcmc.testdata.2<-MCMCglmm(fixed = log(response_time) ~ log(Bigram_Frequency+0.00
                           data= testdata)
 
 summary(mcmc.testdata.2)
+
+library(lme4)
+
+lmer.testdata.1 <- lmer(log(testdata$response_time)~log(testdata$Bigram_Frequency+0.0001) + 
+  testdata$Letters_trailing+testdata$Freq_trailing +testdata$LR_Diversity +testdata$Conc_trailing + 
+  (1|testdata$P) +(1|testdata$Trailing))
+
+summary(lmer.testdata.1)
